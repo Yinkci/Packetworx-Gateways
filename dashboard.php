5671cbeb5baa607f2b3a2ff2746159c91d2dec22
Yinkci <yinkciworks@gmail.com>
2018-11-15 15:13:00 +0800

    update on dashbord

--- dashboard.php
+++ dashboard.php
@@ -1,2 +1,13 @@
 <?php require ("session.php"); ?>
-<?php require ("dashboard-header.php"); ?>
+<?php require ("dashboard-header.php"); 
+$con = pg_connect("host = localhost dbname=tester user=postgres");
+// $con = mysqli_connect("localhost","root","");
+// $db = mysqli_select_db($con,"resume"); 
+if(!$con){
+echo "not connected";
+}
+else{
+	echo "connected";
+}
+
+?>
