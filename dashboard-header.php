fc01c4aafee13b1f65a23b921abc1e843fc52433
Yinkci <yinkciworks@gmail.com>
2018-11-19 15:56:58 +0800

    add search

--- dashboard-header.php
+++ dashboard-header.php
@@ -6,7 +6,7 @@
 			<li><a href="dashboard"><i class="fas fa-code"></i> Dashboards</a></li>
 			<li><a href="gateways"><i class="fas fa-th-list"></i> Gateways</a></li>
 			<li><a href="settings"><i class="fas fa-cog"></i>Settings</a></li>
-			<li><a href="#"><i class="fas fa-wrench"></i> C.A Settings</a></li>
+			<!-- <li><a href="#"><i class="fas fa-wrench"></i> C.A Settings</a></li> -->
 			<li><a href="logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
 
 		</ul>
@@ -46,7 +46,7 @@
 				<li class="<?php if($currentPage=="dashboard"):echo "activelink"; else: endif; ?>"><a href="dashboard"><i class="fas fa-code"></i> Dashboard</a></li>
 				<li class="<?php if($currentPage=="gateways" || $currentPage=="edit-page" || $currentPage=="view"  ):echo "activelink"; else: endif; ?>"><a href="gateways"><i class="fas fa-th-list"></i> Gateways</a></li>
 				<li class="<?php if($currentPage=="settings"):echo "activelink"; else: endif; ?>"><a href="settings"><i class="fas fa-cog"></i> Settings</a></li>
-				<li class="<?php echo $currentPage ?>"><a href="#"><i class="fas fa-wrench"></i> C.A Settings</a></li>
+				<!-- <li class="<?php echo $currentPage ?>"><a href="#"><i class="fas fa-wrench"></i> C.A Settings</a></li> -->
 				<li class="<?php if($currentPage=="logout"):echo "activelink"; else: endif; ?>"><a href="logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
 			</ul>
 		</div>
--- dashboard.php
+++ dashboard.php
@@ -12,93 +12,16 @@
 <body class="dashboard-view">
 
 <div class="menu col-md-10 col-md-2 col-xl-10 d-xl-block pd-left10">
-	<div class="view_gateways" style="overflow-x:auto;">
-		<form action="" method="POST">
-		<table class="gateways-table">
-		<tr class="all_title_tr">
-			<th>ESN</th>
-			<th>Status</th> 
-			<th>Gateway ID</th>
-			<th>SSID</th>
-			<th>Site</th> 
-			<th>City</th> 
-			<th>Action</th> 
-
-		</tr>
-		
-<?php
-
-
-	$query  = mysqli_query($con,"SELECT * FROM  gateways");
-	$row = mysqli_num_rows($query);
-	$x =1;
-	while($rowx = mysqli_fetch_array($query)){
-	$gateway_id = $rowx['gateway_id'];
-	$tti_id = $rowx['tti_id'];
-	$lon = $rowx['lon'];
-	$lat = $rowx['lat'];
-	$display_name = $rowx['display_name'];
-	$deployment_status = $rowx['deployment_status'];
-	$esn = $rowx['esn'];
-	$mac = $rowx['mac'];
-	$serial_number = $rowx['serial_number'];
-	$ctn_box_number = $rowx['ctn_box_number'];
-	$ssid = $rowx['ssid'];
-	$ssid_key = $rowx['ssid_key'];
-	$router_username = $rowx['router_username'];
-	$router_key = $rowx['router_key'];
-	$gateway_key = $rowx['gateway_key'];
-	$owner = $rowx['owner'];
-	$frequency  = $rowx['frequency'];
-	$antenna = $rowx['antenna'];
-	$gateway_site_id = $rowx['gateway_site_id'];
-	$site_location = $rowx['site_location'];
-	$city = $rowx['city'];
-	$contact = $rowx['contact'];
-	$date_installed = $rowx['date_installed'];
-	$care_of = $rowx['care_of']; 
-	$notes = $rowx['notes'];
-	$gateway_username = $rowx['gateway_username'];
-	$monitored = $rowx['monitored'];
-?>
-	<tr class="<?php echo "esn_".$gateway_id ?> clr_<?php echo $x?>">
-			<td><?php echo $esn; ?> </td>
-			<td><?php echo $deployment_status; ?> </td>
-			<td><?php echo $gateway_id; ?> </td>
-			<td><?php echo $ssid; ?> </td>
-			<td><?php echo $site_location; ?> </td>
-			<td><?php echo $city; ?> </td>
-			<td class="action-edit"><a href='<?php echo home_url()."/view/?view=".$esn ?>'> View </a> <a href='<?php echo home_url()."/edit-page/?edit=".$esn ?>'> Edit </a> <a href="javaScript:void(0);"  data-name="<?php echo $gateway_id ?>"  class="deleteGate_"> Delete </a></td>
-	
-		</tr>
-<?php 
-// color counter interval
-if($x==2):
-	$x=0;
-	$x++;
-else:
-	$x++;
-endif;
-} ?><!-- end og while -->
-
-		</table>
-
-		</form>
-	</div>
-<div class="controller">
-	<div class="row">
-		<div class="col-md-3">
-			<div class="gateway_btns"><a href="addgateway">Add New Gateway</a></div>
-		</div>
-		<div class="col-md-3">
-			<div class="">
-				<form method="POST" action="" class="delForm">
-					<input type="submit" name="submit_dellAll" class="delAll" value="Delete All">
-				</form>
+			
+ <div class="d-flex">
+		<div class="log-container text-center">
+			<div class="d-table">
+				<div class="d-cell">
+						<img src="assets/images/logo.png" class="main-dashboard-logo">
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </div><!-- end of col 10 -->
 
 </div><!-- end of row dashbpard-header -->
