7864b2bd42e794ede499f76440804922a6be7996
Yinkci <misakinakahara52@gmail.com>
2018-12-03 14:52:53 +0800

    add minimum password length

--- data/addUser.php
+++ data/addUser.php
@@ -12,16 +12,19 @@ require ("../config/database_con.php");
 	$status = $_POST['status'];
 
 $query_ = pg_query($con,"SELECT * FROM public.user WHERE username='$username'");
-
-	if (pg_num_rows($query_) != 0):
-	  echo "Username already exists";
+if (pg_num_rows($query_) != 0) {
+	echo "Username already exists";
+}
+else{
+	if (strlen($password) < 6):
+	  echo "Password: minimum length of 6 characters";
 	else:
 		$query = pg_query($con,"INSERT INTO public.user(id_user, status, username, password) VALUES ('$id_user','$status','$username','$password')" );
 		pg_close($con);
 		echo "Succesfully added new user";
 		// header("location:".home_url()."/settings");
 	endif;
-
+}
 
   ?>
 
